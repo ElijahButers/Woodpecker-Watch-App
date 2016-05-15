fb68c5a47b3e08259d680e65836fcfd8c815c777
ElijahButers <Elijahbuters@gmail.com>
2016-05-15 14:55:10 +0300

    Updated code in displayError() method that showы the error with animation and then hides it later.

--- Woodpecker WatchKit Extension/Controllers/NewTaskInterfaceController.swift
+++ Woodpecker WatchKit Extension/Controllers/NewTaskInterfaceController.swift
@@ -110,11 +110,16 @@ extension NewTaskInterfaceController {
 extension NewTaskInterfaceController {
   func displayError() {
     errorImage.setHidden(false)
+    
+    animateWithDuration(0.4) {
     self.errorImage.sizeToFitHeight()
+    }
     
     let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(1 * Double(NSEC_PER_SEC)))
     dispatch_after(delayTime, dispatch_get_main_queue()) {
+      self.animateWithDuration(0.4) {
       self.errorImage.setHeight(0)
+    }
     }
   }
 }
